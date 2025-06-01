a5ed0b49568213dacf08f689f833a6040a75a6ca
josueCarreno <150408018+josueCarreno@users.noreply.github.com>
2025-06-01 01:27:40 -0400

    diseño

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 semaforo = false;
 
-document.getElementById('nuevo_producto').addEventListener('click', () => {
+document.querySelectorAll('.nuevo_producto').forEach(agregar => {
+  agregar.addEventListener('click', function() {
+    irArriba();
+    ocultarSecciones("seccion_1");
+    mostrarSecciones("seccion_2");
+    semaforo = true;
+  });
+});
   
-  ocultarSecciones("seccion_1");
-  mostrarSecciones("seccion_2");
-
-  semaforo = true;
+document.querySelectorAll('.volver').forEach(atras => {
+  atras.addEventListener('click', function() {
+    ocultarSecciones("seccion_2");
+    mostrarSecciones("seccion_1");
+  });
 });
 
-document.getElementById('volver').addEventListener('click', () => {
-  ocultarSecciones("seccion_2");
-  mostrarSecciones("seccion_1");
-});
+  
 
 function ocultarSecciones(seccion = "seccion_1") {
   ocultar = document.getElementsByClassName(seccion);
   
   for (elemento of ocultar) {
-    //elemento.classList.remove('flex');
+    elemento.classList.remove('flex');
     elemento.style.display = 'none';
   }
 }
@@ -26,8 +31,15 @@ function mostrarSecciones(seccion = "seccion_2") {
   mostrar = document.getElementsByClassName(seccion);
   
   for (elemento of mostrar) {
-    //elemento.classList.add('flex');
+    elemento.classList.add('flex');
     elemento.classList.remove('hidden');
     elemento.style.display = 'flex';
   }
+}
+
+function irArriba() {
+  window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Animación suave
+  });
 }
